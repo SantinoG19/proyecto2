1de3d66eea0dd7547ae814cfc78b80562ab28dc7
jgottlieb04 <jgottlieb@udesa.edu.ar>
2023-11-08 16:38:55 -0300

    register

--- App.js
+++ App.js
@@ -2,10 +2,9 @@ import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import Register from "./src/screens/Register/Register";
-import Login from "./src/screens/Login/Login";
-import Home from "./src/screens/Home/Home";
-import Menu from "./src/componentes/Menu";
+import Register from './src/screens/Register/Register'
+// import Home from "./src/screens/Home/Home";
+// import Menu from "./src/componentes/Menu";
 
 const Stack = createNativeStackNavigator();
 
