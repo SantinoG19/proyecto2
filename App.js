e6aca6e6d86d8648dbce84a7ce925ed9539e0224
jgottlieb04 <jgottlieb@udesa.edu.ar>
2023-11-18 11:47:49 -0300

    app

--- App.js
+++ App.js
@@ -20,7 +20,7 @@ export default function App() {
         
       <Stack.Screen name="Login" component={Login} options={{ headerShown: false }}/>
       
-      {/* <Stack.Screen name="Menu" component={Menu} options={{ headerShown: false }}/> */}
+      <Stack.Screen name="Menu" component={Menu} options={{ headerShown: false }}/>
 
       
       
